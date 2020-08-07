9fe1ddff7e5056c89cebcd2f403fabfec170345f
Peter <mezeipetister@gmail.com>
2020-08-07 18:02:03 +0200

    ..

---
+++ src/barcode_bridge.rs
@@ -0,0 +1,100 @@
+use std::io::Write;
+use std::io::{BufRead, BufReader};
+use std::process::ChildStdout;
+use std::process::{Command, Stdio};
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
+use std::thread::JoinHandle;
+use websocket::OwnedMessage;
+
+fn start_listener(stdout: ChildStdout, sender: Sender<String>) {
+    let mut f = BufReader::new(stdout);
+    thread::spawn(move || loop {
+        let mut buf = String::new();
+        match f.read_line(&mut buf) {
+            Ok(_) => {
+                // stdin.write_all(b"2\n").unwrap();
+                sender.send(buf).unwrap();
+            }
+            Err(e) => println!("an error!: {:?}", e),
+        }
+    });
+}
+
+fn read_process(rx_stdout: Receiver<String>) -> JoinHandle<()> {
+    thread::spawn(move || {
+        for line in rx_stdout {
+            if let Some(code) = getcode(&line) {
+                println!("{}", code);
+            }
+        }
+    })
+}
+
+fn getcode(from: &str) -> Option<String> {
+    if let Some(start) = from.find("<datalabel>") {
+        if let Some(end) = from.find("</datalabel>") {
+            if start > 0 && end < from.len() {
+                let raw = from[start + 11..end].trim().to_string();
+                let raw_array = raw
+                    .split(" ")
+                    .into_iter()
+                    .map(|c| u8::from_str_radix(c, 16).unwrap())
+                    .collect::<Vec<u8>>();
+                if let Ok(string) = std::str::from_utf8(&raw_array) {
+                    return Some(string.to_string());
+                }
+            }
+        }
+    }
+    None
+}
+
+fn _main() {
+    let cmd = std::env::current_dir().unwrap().join("scanner");
+    let mut child = Command::new(cmd)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to start ping process");
+    let (tx_stdout, rx_stdout) = channel::<String>();
+    // let (tx_stdin, rx_stdin) = channel::<String>();
+    start_listener(child.stdout.unwrap(), tx_stdout);
+    let _ = read_process(rx_stdout);
+
+    let mut input = String::new();
+    let mut stdin = child.stdin.take().unwrap();
+    loop {
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                stdin.write_all(input.as_bytes()).unwrap();
+                input = "".to_string();
+            }
+            Err(error) => println!("error: {}", error),
+        }
+    }
+
+    // read_process.join().unwrap();
+}
+
+pub enum Action {
+    Close,
+    Ping,
+    Text(String),
+    Error,
+    Other,
+    Barcode(String),
+}
+
+pub enum BridgeAction {
+    Error,
+}
+
+//
+//             Channel to send barcode        Message to the bridge via this
+//                           |               /
+pub fn start_barcode_bridge(tx: Sender<Action>) -> Sender<BridgeAction> {
+    let (tx_bridge, rx_bridge) = channel::<BridgeAction>();
+    tx_bridge
+}
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,111 @@
+extern crate hyper;
+extern crate websocket;
+
+// use hyper::Fresh;
+use hyper::Request;
+use hyper::Response;
+use hyper::Server as HttpServer;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::io::Write;
-use std::io::{BufRead, BufReader};
-use std::process::ChildStdout;
-use std::process::{Command, Stdio};
-use std::sync::mpsc::Receiver;
-use std::sync::mpsc::{channel, Sender};
+use std::sync::mpsc::channel;
 use std::thread;
-use std::thread::JoinHandle;
+use websocket::sender::Writer;
+use websocket::sync::Server;
+use websocket::{Message, OwnedMessage};
 
-fn start_listener(stdout: ChildStdout, sender: Sender<String>) {
-    let mut f = BufReader::new(stdout);
-    thread::spawn(move || loop {
-        let mut buf = String::new();
-        match f.read_line(&mut buf) {
-            Ok(_) => {
-                // stdin.write_all(b"2\n").unwrap();
-                sender.send(buf).unwrap();
-            }
-            Err(e) => println!("an error!: {:?}", e),
-        }
-    });
-}
-
-fn read_process(rx_stdout: Receiver<String>) -> JoinHandle<()> {
-    thread::spawn(move || {
-        for line in rx_stdout {
-            if let Some(code) = getcode(&line) {
-                println!("{}", code);
-            }
-        }
-    })
-}
-
-fn getcode(from: &str) -> Option<String> {
-    if let Some(start) = from.find("<datalabel>") {
-        if let Some(end) = from.find("</datalabel>") {
-            if start > 0 && end < from.len() {
-                let raw = from[start + 11..end].trim().to_string();
-                let raw_array = raw
-                    .split(" ")
-                    .into_iter()
-                    .map(|c| u8::from_str_radix(c, 16).unwrap())
-                    .collect::<Vec<u8>>();
-                if let Ok(string) = std::str::from_utf8(&raw_array) {
-                    return Some(string.to_string());
-                }
-            }
-        }
-    }
-    None
-}
+mod barcode_bridge;
+use barcode_bridge::*;
 
 fn main() {
-    let cmd = std::env::current_dir().unwrap().join("scanner");
-    let mut child = Command::new(cmd)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to start ping process");
-    let (tx_stdout, rx_stdout) = channel::<String>();
-    // let (tx_stdin, rx_stdin) = channel::<String>();
-    start_listener(child.stdout.unwrap(), tx_stdout);
-    let _ = read_process(rx_stdout);
-
-    let mut input = String::new();
-    let mut stdin = child.stdin.take().unwrap();
-    loop {
-        match std::io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                stdin.write_all(input.as_bytes()).unwrap();
-                input = "".to_string();
+    // Start listening for WebSocket connections
+    let ws_server = Server::bind("127.0.0.1:2794").unwrap();
+    for connection in ws_server.filter_map(Result::ok) {
+        // Spawn a new thread for each connection.
+        thread::spawn(|| {
+            let (tx, rx) = channel::<Action>();
+            let (tx2, rx2) = channel::<Action>();
+            let tx_for_bridge = tx.clone();
+            let tx_from_bridge = start_barcode_bridge(tx_for_bridge);
+            if !connection
+                .protocols()
+                .contains(&"scannerbridge".to_string())
+            {
+                connection.reject().unwrap();
+                return;
             }
-            Err(error) => println!("error: {}", error),
-        }
-    }
 
-    // read_process.join().unwrap();
+            let mut client = connection.use_protocol("scannerbridge").accept().unwrap();
+            let ip = client.peer_addr().unwrap();
+
+            let (mut ws_rx, mut ws_tx) = client.split().unwrap();
+
+            // Websocket read thread
+            thread::spawn(move || {
+                for message in ws_rx.incoming_messages() {
+                    let message = message.unwrap();
+                    match &message {
+                        OwnedMessage::Close(_) => {
+                            // let message = Message::close();
+                            tx.send(Action::Close).unwrap();
+                            return;
+                        }
+                        OwnedMessage::Ping(data) => {
+                            tx.send(Action::Ping).unwrap();
+                        }
+                        OwnedMessage::Text(data) => {
+                            match &data[..] {
+                                // If we have received error action
+                                "error" => tx.send(Action::Error).unwrap(),
+                                _ => (),
+                            };
+                            // println!("New message {}", data.to_string());
+                        }
+                        _ => {
+                            tx.send(Action::Other).unwrap();
+                        }
+                    }
+                }
+            });
+
+            // Websocket write thread
+            thread::spawn(move || {
+                for action in rx2 {
+                    match action {
+                        Action::Close => {
+                            let message = Message::close();
+                            ws_tx.send_message(&message).unwrap();
+                            println!("Connection closed!");
+                            return;
+                        }
+                        Action::Barcode(code) => {
+                            ws_tx
+                                .send_message(&Message::text(format!("{{\"code\": \"{}\"}}", code)))
+                                .unwrap();
+                        }
+                        _ => {
+                            // Doing nothing
+                        }
+                    }
+                }
+            });
+
+            // Main process
+            let handle_main = thread::spawn(move || {
+                for action in rx {
+                    match action {
+                        Action::Close => {
+                            tx2.send(Action::Close).unwrap();
+                            return;
+                        }
+                        Action::Barcode(code) => tx2.send(Action::Barcode(code)).unwrap(),
+                        Action::Error => tx_from_bridge.send(BridgeAction::Error).unwrap(),
+                        _ => tx2.send(Action::Other).unwrap(),
+                    }
+                }
+            });
+
+            let _ = handle_main.join();
+        });
+    }
 }
