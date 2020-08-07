81eb9dd571febf2e8dfcf0e0e2dba5f98df4f1de
Peter <mezeipetister@gmail.com>
2020-08-07 12:11:55 +0200

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,58 @@
+use std::io::Write;
 use std::io::{BufRead, BufReader};
+use std::process::Child;
+use std::process::{ChildStdin, ChildStdout};
 use std::process::{Command, Stdio};
+use std::sync::mpsc::Receiver;
 use std::sync::mpsc::{channel, Sender, TryRecvError};
 use std::thread;
-use std::thread::sleep;
-use std::time::Duration;
+use std::thread::JoinHandle;
 
-fn start_listener(sender: Sender<String>) {
-    let cmd = std::env::current_dir().unwrap().join("scanner");
-    let child = Command::new(cmd)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to start ping process");
-
-    println!("Started process: {}", child.id());
-
-    thread::spawn(move || {
-        let mut f = BufReader::new(child.stdout.unwrap());
-        loop {
-            let mut buf = String::new();
-            match f.read_line(&mut buf) {
-                Ok(_) => {
-                    sender.send(buf).unwrap();
-                }
-                Err(e) => println!("an error!: {:?}", e),
+fn start_listener(stdout: ChildStdout, sender: Sender<String>, receiver: Receiver<String>) {
+    let mut f = BufReader::new(stdout);
+    thread::spawn(move || loop {
+        let mut buf = String::new();
+        match f.read_line(&mut buf) {
+            Ok(_) => {
+                // stdin.write_all(b"2\n").unwrap();
+                sender.send(buf).unwrap();
             }
+            Err(e) => println!("an error!: {:?}", e),
         }
     });
 }
 
-fn main() {
-    let (tx, rx) = channel();
-    start_listener(tx);
+fn read_process(rx_stdout: Receiver<String>) -> JoinHandle<()> {
+    thread::spawn(move || {
+        for line in rx_stdout {
+            println!("Ok {}", line);
+        }
+    })
+}
 
+fn main() {
+    let cmd = std::env::current_dir().unwrap().join("scanner");
+    let mut child = Command::new(cmd)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to start ping process");
+    let (tx_stdout, rx_stdout) = channel::<String>();
+    let (tx_stdin, rx_stdin) = channel::<String>();
+    start_listener(child.stdout.unwrap(), tx_stdout, rx_stdin);
+    let _ = read_process(rx_stdout);
+
+    let mut input = String::new();
+    let mut stdin = child.stdin.take().unwrap();
     loop {
-        match rx.try_recv() {
-            Ok(line) => {
-                // Ok response from the scanner
-                println!("{}", line);
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                stdin.write_all(input.as_bytes()).unwrap();
+                input = "".to_string();
             }
-            Err(TryRecvError::Empty) => {
-                // sleep(Duration::from_secs(1)); // ??
-                continue;
-            }
-            Err(e) => {
-                panic!(e);
-            }
+            Err(error) => println!("error: {}", error),
         }
     }
+
+    // read_process.join().unwrap();
 }
